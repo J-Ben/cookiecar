3a6054aa83fd8eabfd49d8bafba2009f2940de20
Saleck-Eljili <salek.eljili@gmail.com>
2023-03-30 10:37:23 +0200

    feat: init project

---
+++ src/main/java/com/example/cookiecar/CookiecarApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookiecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookiecarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookiecarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cookiecar/CookiecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookiecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiecarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
